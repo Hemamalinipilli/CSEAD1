bf98baeed677aa3a61aaf836459dfbfaf8918838
Hemamalinipilli <84316344+Hemamalinipilli@users.noreply.github.com>
2022-10-22 09:44:57 +0530

    one more time

--- src/csea/demo1.java
+++ src/csea/demo1.java
@@ -5,6 +5,8 @@ public class demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("welcome to devops");
+System.out.println("welcome to devops1");
+
 	}
 
 }
